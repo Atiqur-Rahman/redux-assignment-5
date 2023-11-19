988670841e94600c2f987e2afb67c34fa4f4f360
Atiqur-Rahman <rahmanatique88@gmail.com>
2023-11-20 00:03:52 +0600

    assignment is completed

--- rtk/videos/thunk.js
+++ rtk/videos/thunk.js
@@ -1,12 +1,25 @@
 const { createAsyncThunk } = require('@reduxjs/toolkit');
-const { default: fetch } = require('node-fetch');
+const fetch = require('node-fetch');
 
 const fetchRelatedVideos = createAsyncThunk('video/fetchRelatedVideos', async (queryText) => {
     const response = await fetch(`http://localhost:9000/videos?${queryText}`);
     const data = await response.json();
 
-    data.sort(function (a, b) {
-        return parseFloat(views[b]) - parseFloat(views[a]);
+    // data.sort(function (a, b) {
+    //     return parseFloat(a.views) - parseFloat(b.views);
+    // });
+
+    data.sort((a, b) => {
+        const viewsA = parseFloat(a.views);
+        const viewsB = parseFloat(b.views);
+
+        if (viewsA > viewsB) {
+            return -1;
+        } else if (viewsA < viewsB) {
+            return 1;
+        } else {
+            return 0;
+        }
     });
 
     return data;
--- rtk/videos/videoSlice.js
+++ rtk/videos/videoSlice.js
@@ -1,4 +1,5 @@
 const { createSlice } = require('@reduxjs/toolkit');
+const { fetchVideos, fetchRelatedVideos } = require('./thunk');
 
 // initial State
 const initialState = {
@@ -11,7 +12,41 @@ const initialState = {
 const videoSlice = createSlice({
     name: 'video',
     initialState,
-    // extraReducers: (builder) => {
-    //     builder.addCase()
-    // }
+    extraReducers: (builder) => {
+        builder.addCase(fetchVideos.pending, (state, action) => {
+            state.loading = true;
+            state.error = '';
+        });
+        builder.addCase(fetchVideos.fulfilled, (state, action) => {
+            state.loading = false;
+            state.videos = action.payload;
+            state.error = '';
+            state.objectLoaded = true;
+        });
+        builder.addCase(fetchVideos.rejected, (state, action) => {
+            state.loading = false;
+            state.videos = [];
+            state.error = action.error.message;
+            state.objectLoaded = false;
+        });
+        builder.addCase(fetchRelatedVideos.pending, (state, action) => {
+            state.loading = true;
+            state.error = '';
+        });
+        builder.addCase(fetchRelatedVideos.fulfilled, (state, action) => {
+            state.loading = false;
+            state.videos = action.payload;
+            state.error = '';
+            state.objectLoaded = true;
+        });
+        builder.addCase(fetchRelatedVideos.rejected, (state, action) => {
+            state.loading = false;
+            state.videos = [];
+            state.error = action.error.message;
+            state.objectLoaded = false;
+        });
+    },
 });
+
+module.exports = videoSlice.reducer;
+module.exports.videoSliceActions = videoSlice.actions;
